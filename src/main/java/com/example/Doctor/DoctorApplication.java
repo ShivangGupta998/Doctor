eceb5086d31abadcdc3a949a3f6a04bb8c390d88
Shivang Gupta <shivanggupta998@gmail.com>
2025-02-27 16:49:59 +0530

    Update DoctorApplication.java
    update

--- src/main/java/com/example/Doctor/DoctorApplication.java
+++ src/main/java/com/example/Doctor/DoctorApplication.java
@@ -1,13 +1,10 @@
-package com.example.Doctor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DoctorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DoctorApplication.class, args);
-	}
-
+public class DoctorApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorApplication.class, args);
+    }
 }
